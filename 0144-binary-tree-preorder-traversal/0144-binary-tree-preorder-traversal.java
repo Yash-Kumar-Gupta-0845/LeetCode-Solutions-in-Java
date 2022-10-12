da9ee96e76641ab7dbec5aa86a0832b768c1a765
Yash Kumar Gupta <87761477+Yash-Kumar-Gupta-0845@users.noreply.github.com>
2022-10-12 08:47:43 +0530

    Time: 1 ms (49.71%), Space: 42.5 MB (28.32%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -16,15 +16,22 @@
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
         // visiting every node once
+        // stack first store root then right and then left because we have to return left value first.
+        
         List <Integer> list = new ArrayList<Integer>();
-        dfs(root,list);
-            return list;
-    }
-        private void dfs(TreeNode node, List<Integer> list){
-            if ( node == null)
-                return;
-            list.add(node.val);
-            dfs(node.left,list);
-            dfs(node.right, list);
+        Stack<TreeNode> rights = new Stack<TreeNode>();
+        while (root != null){
+            list.add(root.val);
+            if (root.right != null){
+                rights.push (root.right);
+            }
+            if (root.left == null && !rights.isEmpty()){
+                root = rights.pop();
+            }
+            else{
+                root = root.left;
+            }
         }
+        return list;
+    }
 }
