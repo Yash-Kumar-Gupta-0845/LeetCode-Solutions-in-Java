80c5e8aa1dfd537df023d81977e918f47e3e434e
Yash Kumar Gupta <87761477+Yash-Kumar-Gupta-0845@users.noreply.github.com>
2022-10-13 08:45:03 +0530

    Time: 1 ms (47.15%), Space: 42.5 MB (19.72%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -15,16 +15,31 @@
  */
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> list = new ArrayList<Integer>();
-        dfs(root,list);
-        return list;
-    }
+//         List<Integer> list = new ArrayList<Integer>();
+//         dfs(root,list);
+//         return list;
+//     }
     
-    private void dfs(TreeNode node, List<Integer> list){
-            if ( node == null)
-                return;
-            dfs(node.left,list);
-            list.add(node.val);
-            dfs(node.right, list);
+//     private void dfs(TreeNode node, List<Integer> list){
+//             if ( node == null)
+//                 return;
+//             dfs(node.left,list);
+//             list.add(node.val);
+//             dfs(node.right, list);
+        List<Integer> list = new ArrayList<Integer>();
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        TreeNode temp = root;
+        while(!stack.isEmpty() || root != null){
+            if ( root != null){
+                stack.push(root);
+                root = root.left;
+            }
+            else{
+                root = stack.pop();
+                list.add(root.val);
+                root= root.right;
+            }
+        }
+        return list;
         }
 }
