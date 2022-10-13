36da8bb7b5ca697ccc12d16ace280ab6ea947448
Yash Kumar Gupta <87761477+Yash-Kumar-Gupta-0845@users.noreply.github.com>
2022-10-14 01:25:17 +0530

    Time: 1 ms (47.14%), Space: 40.4 MB (94.42%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -26,20 +26,43 @@ class Solution {
 //             dfs(node.left,list);
 //             list.add(node.val);
 //             dfs(node.right, list);
+        
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        
+                    // Way 2
+        
+        
+        // List<Integer> list = new ArrayList<Integer>();
+        // Stack<TreeNode> stack = new Stack<TreeNode>();
+        // TreeNode temp = root;
+        // while(!stack.isEmpty() || root != null){
+        //     if ( root != null){
+        //         stack.push(root);
+        //         root = root.left;
+        //     }
+        //     else{
+        //         root = stack.pop();
+        //         list.add(root.val);
+        //         root= root.right;
+        //     }
+        // }
+        // return list;
+        
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        
+                    //Way3
         List<Integer> list = new ArrayList<Integer>();
-        Stack<TreeNode> stack = new Stack<TreeNode>();
-        TreeNode temp = root;
-        while(!stack.isEmpty() || root != null){
-            if ( root != null){
-                stack.push(root);
-                root = root.left;
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode current = root;
+        while (current != null || !stack.isEmpty()){
+            while( current != null){
+                stack.push(current);
+                current = current.left;
             }
-            else{
-                root = stack.pop();
-                list.add(root.val);
-                root= root.right;
-            }
+            current = stack.pop();
+            list.add(current.val);
+            current = current.right;
         }
         return list;
-        }
+    }
 }
