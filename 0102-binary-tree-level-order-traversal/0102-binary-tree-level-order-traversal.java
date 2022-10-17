fcb456b6d8d074931b14e98f06b99f1c4c6f94dd
Yash Kumar Gupta <87761477+Yash-Kumar-Gupta-0845@users.noreply.github.com>
2022-10-17 11:07:09 +0530

    Time: 2 ms (50.65%), Space: 43.1 MB (79.38%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -15,19 +15,45 @@
  */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> list = new ArrayList<>();
-        levelHelper(list, root, 0);
-        return list;
-    }
-    public void levelHelper(List<List<Integer>> res, TreeNode root, int height){
-        if ( root == null)
-            return;
-        if ( height >= res.size()){
-            List<Integer> newLevel = new LinkedList<Integer>();
-            res.add(newLevel);
+        //~~~~~~~~~~~ Way 1 ~~~~~~~~~~~~~~
+        
+        
+//         List<List<Integer>> list = new ArrayList<>();
+//         levelHelper(list, root, 0);
+//         return list;
+//     }
+//     public void levelHelper(List<List<Integer>> res, TreeNode root, int height){
+//         if ( root == null)
+//             return;
+//         if ( height >= res.size()){
+//             List<Integer> newLevel = new LinkedList<Integer>();
+//             res.add(newLevel);
+//         }
+//         res.get(height).add(root.val);
+//         levelHelper(res, root.left, height+1);
+//         levelHelper(res, root.right, height+1);
+        // ~~~~~~~~~~~~~~~~~~~~ WAY @ ~~~~~~~~~~~~~~
+        
+        List<List<Integer>> res = new ArrayList<>();
+        if( root == null)
+            return res;
+        Queue <TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while(!queue.isEmpty()){
+            List<Integer> level = new ArrayList<>();
+            int cnt = queue.size();
+            for ( int i =0; i<cnt ; i++){
+                TreeNode node = queue.poll();
+                level.add(node.val);
+                if (node.left != null){
+                    queue.add(node.left);
+                }
+                if (node.right != null){
+                    queue.add(node.right);
+                }
+            }
+            res.add(level);
         }
-        res.get(height).add(root.val);
-        levelHelper(res, root.left, height+1);
-        levelHelper(res, root.right, height+1);
-    } 
+        return res;
+     } 
 }
