153fdd5aecfc7ff3c0efa17fcaf67dfbdc362f14
Yash Kumar Gupta <87761477+Yash-Kumar-Gupta-0845@users.noreply.github.com>
2023-01-22 17:24:17 +0530

    Time: 2 ms (41.45%), Space: 41 MB (37.39%) - LeetHub

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -1,17 +1,22 @@
 class Solution {
     public int addDigits(int num) {
-        int res = unitFinder(num);
-        while (res > 9){
-            res = unitFinder(res);
-        }
-        return res;
-    }
-    private int unitFinder(int n){
-        int dSum = 0;
-        while(n>0){
-            dSum+=n%10;
-            n/=10;
-        }
-        return dSum;
+        if (num < 9)
+            return num;
+        if (num % 9 == 0)
+            return 9;
+        return num%9;
+    //     int res = unitFinder(num);
+    //     while (res > 9){
+    //         res = unitFinder(res);
+    //     }
+    //     return res;
+    // }
+    // private int unitFinder(int n){
+    //     int dSum = 0;
+    //     while(n>0){
+    //         dSum+=n%10;
+    //         n/=10;
+    //     }
+    //     return dSum;
     }
 }
