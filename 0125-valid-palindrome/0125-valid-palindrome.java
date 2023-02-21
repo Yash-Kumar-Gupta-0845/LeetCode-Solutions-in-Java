b37d4ad85f56cd121d3ce34171a07392dd088c10
Yash Kumar Gupta <87761477+Yash-Kumar-Gupta-0845@users.noreply.github.com>
2023-02-21 10:15:14 +0530

    Time: 3 ms (96.77%), Space: 42.2 MB (82.17%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,34 +1,21 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        String str = s.toLowerCase();
-        String nonCharacter = str.replaceAll("([^a-z0-9])","");
-        String rev = "";
-        for (int i = nonCharacter.length()-1; i >= 0 ;i--){
-            rev+=nonCharacter.charAt(i);
+         int left = 0;
+         int right = s.length()-1;
+        while (left < right){
+            if (! Character.isLetterOrDigit(s.charAt(left)))
+                left++;
+             else if (! Character.isLetterOrDigit(s.charAt(right)))
+                right--;
+            else{
+                if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right)))
+                    return false;
+                 left++;
+                 right--;
+            }
         }
-        if (nonCharacter.equals(rev))
-            return true;
-        else
-            return false;
+        return true;
+        
+        
     }
-    // public boolean isPalindrom(String s){
-    //     String Character = "";
-    //     int left = 0;
-    //      int right = s.length()-1;
-    //     while (left <= right){
-    //         if (! Character.isLetterOrDigit(s.charAt(left)))
-    //             left++;
-    //         else if (! Character.isLetterOrDigit(s.charAt(left)))
-    //             right--;
-    //         else{
-    //             if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))){
-    //                 return false;
-    //                 break;
-    //             }
-    //              left++;
-    //              right--;
-    //         }
-    //     }
-    //     return true;
-    // }
 }
