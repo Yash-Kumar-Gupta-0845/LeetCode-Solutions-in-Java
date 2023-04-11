0b947cbba95fbeeb730636c030e0ec1c590bd11c
Yash Kumar Gupta <87761477+Yash-Kumar-Gupta-0845@users.noreply.github.com>
2023-04-11 09:49:35 +0530

    Time: 1 ms (100.00%), Space: 39.5 MB (88.33%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.java
+++ 0441-arranging-coins/0441-arranging-coins.java
@@ -1,21 +1,5 @@
 class Solution {
     public int arrangeCoins(int n) {
-        // if (n == 1 || n == 2)
-        //     return 1;
-        // int maxCoin = ( n%2 == 0 ? n/2 : n/2+1);
-        //     return maxCoin -1;
-        long left = 0, right = n;
-        while(left <= right){
-            long mid = left + (right - left)/2;
-            long k = mid*(mid+1)/2;
-            if(k == n)
-                return (int)mid;
-            else if (k>n){
-                right = mid-1;
-            }
-            else
-                left = mid+1;
-        }
-        return (int)right;
+        return (int)(Math.sqrt(2*(long)n + 0.25)- 0.5);
     }
 }
