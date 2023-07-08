0fadc65a927176e062af46ad3886d194d8a08e5b
Yash Kumar Gupta <87761477+Yash-Kumar-Gupta-0845@users.noreply.github.com>
2023-07-08 23:49:18 +0530

    Added solution - LeetHub

---
+++ Find triplets with zero sum - GFG/find-triplets-with-zero-sum.java
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+import java.util.*;
+class Triplets{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+			int n=sc.nextInt();
+			int[] a=new int[n];
+			for(int i=0;i<n;i++){
+				a[i]=sc.nextInt();
+			}
+			Solution g=new Solution();
+			if(g.findTriplets(a,n))
+				System.out.println("1");
+			else
+				System.out.println("0");
+			
+		}
+	}
+}
+// } Driver Code Ends
+
+
+/*Complete the function below*/
+
+
+class Solution
+{
+    // arr[]: input array
+    // n: size of the array
+    //Function to find triplets with zero sum.
+	public boolean findTriplets(int arr[] , int n)
+    {
+        boolean found = false;
+        for (int i = 0; i < n - 1; i++) {
+            HashSet<Integer> s = new HashSet<Integer>();
+            for (int j = i + 1; j < n; j++) {
+                int x = -(arr[i] + arr[j]);
+                if (s.contains(x)) {
+                    found = true;
+                }
+                else {
+                    s.add(arr[j]);
+                }
+            }
+        }
+        return found;
+    }
+}
