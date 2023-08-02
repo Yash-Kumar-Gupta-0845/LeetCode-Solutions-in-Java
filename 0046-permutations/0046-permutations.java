20292290d56fb942ec9c89fea5e3c81732020d4d
Yash Kumar Gupta <87761477+Yash-Kumar-Gupta-0845@users.noreply.github.com>
2023-08-02 19:12:37 +0530

    Time: 3 ms (10.08%), Space: 44.3 MB (12.89%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,25 +1,20 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> list = new ArrayList<>();
-        List<Integer> sublist = new ArrayList<>();
-        boolean[] visited = new boolean[nums.length];
-        backtrack(list,sublist,nums,visited);
+        helper(new ArrayList<>(), list, nums);
         return list;
     }
-    
-    public void backtrack(List<List<Integer>> list, List<Integer> sublist, int nums[], boolean[] visited){
-        if (sublist.size() == nums.length){
-            list.add(new ArrayList<>(sublist));
+    public void helper(List<Integer> curr, List<List<Integer>> list, int[] num){
+        if (curr.size() == num.length){
+            list.add(new ArrayList<>(curr));
             return;
         }
-        for (int i = 0; i < nums.length ; i++){
-            if (visited[i] == true)
-                continue;
-            sublist.add(nums[i]);
-            visited[i] = true;
-            backtrack(list,sublist,nums,visited);
-            sublist.remove(sublist.size()-1);
-            visited[i] = false;
+        for(int nums:num){
+            if (!curr.contains(nums)){
+                curr.add(nums);
+                helper(curr,list,num);
+                curr.remove(curr.size() - 1);
+            }
         }
     }
 }
